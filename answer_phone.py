04f570f94e8832ee47c99b5eb5f66f0b5fdf9216
carlo3247 <cawinkelhake@aol.com>
2019-02-02 21:57:02 -0500

    combined stt with twilio

--- answer_phone.py
+++ answer_phone.py
@@ -2,11 +2,33 @@ from flask import Flask
 from flask import request
 from twilio.twiml.voice_response import VoiceResponse
 
+import urllib.request
+
+from google.cloud import speech
+from google.cloud.speech import enums
+from google.cloud.speech import types
+
 app = Flask(__name__)
+
+stt_client = speech.SpeechClient()
 
 @app.route("/recorded", methods=['GET', 'POST'])
 def recorded():
+    # get url
     print('url: {}'.format(request.form['RecordingUrl']))
+    url = request.form['RecordingUrl']
+
+    # get .wav file
+    audio_file = urllib.request.urlopen(url)
+    content = audio_file.read()
+
+    # stt using google cloud
+    audio = types.RecognitionAudio(content=content)
+    config = types.RecognitionConfig(language_code='en-US')
+    stt_response = stt_client.recognize(config, audio)
+
+    print(stt_response[0].alternatives[0])
+
     response = VoiceResponse()
     response.say('Thank you')
     response.hangup()
