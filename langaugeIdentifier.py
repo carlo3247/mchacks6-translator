08cbb8b4f2805fad64aad0ca04ece43253b52ddb
BradMcBain <bradmcbaineng@gmail.com>
2019-02-03 01:11:21 -0500

    Updated with language codes

--- langaugeIdentifier.py
+++ langaugeIdentifier.py
@@ -4,7 +4,6 @@ def findLang(inputString):
     listOfWords= re.split('\W+', inputString)
     print(listOfWords)
     languageList = ['arabic', 
-                'catalan', 
                 'chinese', 
                 'czech', 
                 'danish', 
@@ -13,7 +12,6 @@ def findLang(inputString):
                 'french', 
                 'german', 
                 'hindi', 
-                'hungarian', 
                 'italian', 
                 'japanese', 
                 'korean', 
@@ -24,17 +22,42 @@ def findLang(inputString):
                 'spanish',
                 'swedish',
                 'turkish']
+       
+    translatorList = {'arabic': 'ar',
+                    'chinese': 'zh',
+                    'czech': 'cs', 
+                    'danish': 'da', 
+                    'dutch': 'nl',
+                    'finnish': 'fi', 
+                    'french': 'fr', 
+                    'german': 'de', 
+                    'hindi': 'hi', 
+                    'italian': 'it', 
+                    'japanese': 'ja', 
+                    'korean': 'ko', 
+                    'norwegian': 'nb', 
+                    'polish': 'pl', 
+                    'portuguese': 'pt',
+                    'russian': 'ru',
+                    'spanish': 'es',
+                    'swedish': 'sv',
+                    'turkish': 'tr'}
     flag = 0
+    result = ''
     for x in range(0, len(listOfWords)):
         for y in range(0, len(languageList)):
             if(languageList[y] == listOfWords[x]):
-                print('This is ' + languageList[y])
+                result = translatorList[languageList[y]]
+                #print('This is ' + languageList[y])
+                #print('It corresponds to ' + result)
                 flag = 1
         if(flag == 1):
             break 
-       
+    
+    return result
+
 def main():
-    findLang('english Arabic chinese bruh dude wow arabic woah crazy ')
+    findLang('hello I am russian, not french')
     
 if __name__ == '__main__':
     main()
