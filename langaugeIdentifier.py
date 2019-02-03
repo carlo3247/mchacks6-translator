053445efe0502fce49710180a93f297250bf292a
carlo3247 <cawinkelhake@aol.com>
2019-02-03 02:10:05 -0500

    changed list to set

--- langaugeIdentifier.py
+++ langaugeIdentifier.py
@@ -1,43 +1,36 @@
 import re
-def findLang(inputString):
-    inputString = inputString.lower();
-    listOfWords= re.split('\W+', inputString)
-    #print(listOfWords)
 
-    translatorList = {'arabic': 'ar',
-                    'chinese': 'zh',
-                    'czech': 'cs', 
-                    'danish': 'da', 
-                    'dutch': 'nl',
-                    'finnish': 'fi', 
-                    'french': 'fr', 
-                    'german': 'de', 
-                    'hindi': 'hi', 
-                    'italian': 'it', 
-                    'japanese': 'ja', 
-                    'korean': 'ko', 
-                    'norwegian': 'nb', 
-                    'polish': 'pl', 
-                    'portuguese': 'pt',
-                    'russian': 'ru',
-                    'spanish': 'es',
-                    'swedish': 'sv',
-                    'turkish': 'tr'}
-    flag = 0
-    result = ''
-    for x in range(0, len(listOfWords)):
-        for key in translatorList:
-            if(key == listOfWords[x]):
-                result = translatorList[key]
-                print(result)
-                flag = 1
-        if(flag == 1):
-            break 
-    
-    return result
+translatorList = {
+                'arabic': 'ar',
+                'chinese': 'zh',
+                'czech': 'cs', 
+                'danish': 'da', 
+                'dutch': 'nl',
+                'finnish': 'fi', 
+                'french': 'fr', 
+                'german': 'de', 
+                'hindi': 'hi', 
+                'italian': 'it', 
+                'japanese': 'ja', 
+                'korean': 'ko', 
+                'norwegian': 'nb', 
+                'polish': 'pl', 
+                'portuguese': 'pt',
+                'russian': 'ru',
+                'spanish': 'es',
+                'swedish': 'sv',
+                'turkish': 'tr'
+                }
+
+def findLang(inputString):
+    listOfWords= re.split('\W+', inputString.lower())
+    for word in re.split('\W+', inputString.lower()):
+        if word in translatorList:
+            return translatorList.get(word)
+    return ''
 
 def main():
-    findLang('Hello I am english, please translate this to russian not korean')
+    print(findLang('Hello I am english, please translate this to russian not korean'))
     
 if __name__ == '__main__':
     main()
